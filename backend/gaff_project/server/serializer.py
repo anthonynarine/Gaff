b416ac9707a998956100e64423a9e57cee85e748
anthonynarine <anarine83@gmail.com>
2023-07-28 10:36:55 -0400

    added a serializer class for Channel. This will allow us to use the channel fk associated with the Server (the refrence name is used - see model) to bring in all the channels on that server

--- backend/gaff_project/server/serializer.py
+++ backend/gaff_project/server/serializer.py
@@ -1,9 +1,18 @@
 
 from rest_framework import serializers
-from .models import Category, Server
+from .models import Category, Server, Channel
+
+class ChannelSerializer(serializers.ModelSerializer):
+    # Server data will also incude all categories on that server
+    class Meta:
+        model = Channel
+        fields = "__all__"
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerializer(many=True)
+    # Server data will also include all Categories on that Sever by refrence of the fk.
+    
     
     class Meta:
         model = Server
