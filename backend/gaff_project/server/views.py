16e35e7ba75cbf36b2392afd50e830d055fb3e76
anthonynarine <anarine83@gmail.com>
2023-07-28 09:57:19 -0400

    ServerListViewSet class now has a list method that filters by category, numof result and by user. all utilizing the queryset class method

--- backend/gaff_project/server/views.py
+++ backend/gaff_project/server/views.py
@@ -10,9 +10,18 @@ class ServerListViewSet(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get("category")
+        num_results = request.query_params.get("num_results")
+        by_user = request.query_params.get("by_user") == "true"
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+        
+        if num_results:
+            self.queryset = self.queryset[: int(num_results)]  
+            
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)  
+            
         serializer = ServerSerializer(self.queryset, many=True)
-
         return Response(serializer.data)
