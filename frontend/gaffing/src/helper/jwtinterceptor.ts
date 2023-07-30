80432eb49e5decb8d90c712d60dc1d4599f9f96c
anthonynarine <anarine83@gmail.com>
2023-07-30 11:55:37 -0400

    useCrud hook set up to use the axios interceptor to fetch data and store in state. I need to combine all the use state calls in a refactor

--- frontend/gaffing/src/helper/jwtinterceptor.ts
+++ frontend/gaffing/src/helper/jwtinterceptor.ts
@@ -1,27 +1,41 @@
-import { BASE_URL } from "../config";
 import axios, { AxiosInstance } from "axios";
 import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../config";
 
-const API_BASE_URL = BASE_URL;
 
+/**
+ * Custom hook to create an Axios instance with an interceptor for handling authentication errors.
+ *
+ * @returns {AxiosInstance} The Axios instance with the interceptor.
+ */
 const useAxiosWithInterceptor = (): AxiosInstance => {
+  // Get the API base URL from the config.
+  const API_BASE_URL = BASE_URL;
+
+  // Create a new Axios instance with the API base URL.
   const jwtAxios = axios.create({ baseURL: API_BASE_URL });
+
+  // Get the `navigate` function from react-router-dom.
   const navigate = useNavigate();
 
-  jwtAxios.interceptors.response.use((response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
-    if(error.response?.status === 403){
-        const goHome = () => navigate("/test")
+  // Add an Axios interceptor to handle authentication errors.
+  jwtAxios.interceptors.response.use(
+    (response) => {
+      // If the response is successful, simply return it.
+      return response;
+    },
+    async (error) => {
+      // If the response status is 401 (Unauthorized), redirect the user to the home page.
+      if (error.response?.status === 401) {
+        const goHome = () => navigate("/");
         goHome();
+      }
     }
-    throw error
-  }
-  )
+  );
+
+  // Return the Axios instance with the interceptor.
   return jwtAxios;
-  
 };
 
 export default useAxiosWithInterceptor;
+
