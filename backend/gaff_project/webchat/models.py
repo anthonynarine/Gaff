76ef67983c04f75dc00efdcdda49666f17fe546d
anthonynarine <anarine83@gmail.com>
2023-08-08 22:12:43 -0400

    Conversation and Message models created in webchat app as part of storing the user message history

--- backend/gaff_project/webchat/models.py
+++ backend/gaff_project/webchat/models.py
@@ -1,3 +1,15 @@
+from enum import auto
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+class Conversation(models.Model):
+    channel_id = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class Message(models.Model):
+    Conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="message")
+    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
