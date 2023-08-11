6385243ba9633adceb23e1f80654b2c64dc2061e
anthonynarine <anarine83@gmail.com>
2023-08-11 11:23:31 -0400

    completely refactored the consumer class using chatGPT the info is better organized and some methods were privatized using the _ prior to the method name (they are't truly private though as python does not truly does this

--- backend/gaff_project/webchat/consumer.py
+++ backend/gaff_project/webchat/consumer.py
@@ -1,20 +1,26 @@
 from channels.generic.websocket import JsonWebsocketConsumer
 from asgiref.sync import async_to_sync
+from .models import Conversation, Message
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class WebChatConsumer(JsonWebsocketConsumer):
     """
-    WebSocket consumer that manages real-time chat interactions over WebSocket connections.
-    This consumer handles the lifecycle of WebSocket connections including connection,
-    message reception, message sending, and disconnection. The underlying WebSocket handshake
-    and protocol specifics are abstracted by the Django Channels library.
+    WebSocket consumer facilitating real-time chat using Django Channels. 
+
+    On connecting, the frontend establishes a WebSocket connection to a specific chat room.
+    Messages from the frontend are received in real-time, saved to the database, 
+    and then broadcasted to all clients in the chat room.
+
+    Methods:
+    - connect: Sets up the WebSocket connection and joins the specified chat room.
+    - receive_json: Handles incoming messages, saves them, and broadcasts to all clients in the room.
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Initialize the consumer and set up the default chat room.
-        
-        Attributes:
-            room_name (str): The name of the chat room this consumer instance is associated with.
+        Constructs the consumer, initializing the chat room ID and user attributes.
         """
         super().__init__(*args, **kwargs)
         self.channel_id = None
@@ -22,44 +28,78 @@ class WebChatConsumer(JsonWebsocketConsumer):
 
     def connect(self):
         """
-        Called immediately after a successful WebSocket handshake. 
-        
-        This method:
-        1. Accepts the WebSocket connection.
-        2. Adds the socket to the chat room group for message broadcasting.
-        
-        The actual handshake mechanics are handled internally by Django Channels.
+        Set up the WebSocket connection. The channel ID is derived from the route.
+        The user is then associated (currently hardcoded) and the connection is added to the chat group.
         """
         self.accept()
-        self.channel_id = self.scope["url_route"]["kwargs"]["channelId"]
-        print(self.scope)
-        async_to_sync(self.channel_layer.group_add)(self.channel_id, self.channel_name)
+        self._extract_channel_id_from_route()
+        self._associate_user()
+        self._join_chat_group()
 
     def receive_json(self, content):
         """
-        Handles incoming JSON content (messages) from the client/front end.
-
-        Args:
-            content (dict): The message received from the client/front end.
+        Processes incoming messages from the frontend.
         
-        This method processes the incoming message and broadcasts it to all clients
-        in the associated chat room. The message format is transformed slightly during this process.
+        The received content is expected in the format:
+        {
+            "message": "text of the chat message"
+        }
+
+        On processing, the message is saved to the database and broadcasted in the format:
+        {
+            "type": "chat.message",
+            "id": "<message ID>",
+            "sender": "<message content>",
+            "timestamp": "<timestamp>",
+            "new_message": "<text of the chat message>"
+        }
         """
+        message_text = content.get("message")
+        if not message_text:
+            return  # Handle cases where the message content might be missing or null
+        
+        new_message = self._save_received_message(message_text)
+        self._broadcast_message_to_group(new_message)
+
+    def _extract_channel_id_from_route(self):
+        """Retrieve channel ID from the URL route."""
+        self.channel_id = self.scope["url_route"]["kwargs"].get("channelId")
+
+    def _associate_user(self):
+        """Associate the connection with a user (currently hardcoded)."""
+        self.user = User.objects.get(id=1)  # Placeholder logic
+
+    def _join_chat_group(self):
+        """Add the current connection to the chat group."""
+        async_to_sync(self.channel_layer.group_add)(self.channel_id, self.channel_name)
+
+    def _save_received_message(self, message_text):
+        """Save the received message in the database."""
+        conversation, _ = Conversation.objects.get_or_create(channel_id=self.channel_id)
+        return Message.objects.create(
+            conversation=conversation, sender=self.user, content=message_text
+        )
+
+    def _broadcast_message_to_group(self, message):
+        """Send the message to all members of the chat group."""
         async_to_sync(self.channel_layer.group_send)(
             self.channel_id,
             {
-                "type": "chat.message", 
-                "new_message": content["message"]
+                "type": "chat.message",
+                "id": message.id,
+                "sender": message.content,
+                "timestamp": message.timestamp.isoformat(),
+                "new_message": message.content,
             },
         )
-
+        
     def chat_message(self, message):
         """
         Sends a chat message to the client/front end.
 
         Args:
             message (dict): The message to be sent to the client.
-            
+
         This method is responsible for forwarding chat messages to the client after
         they have been received and processed.
         """
@@ -68,13 +108,50 @@ class WebChatConsumer(JsonWebsocketConsumer):
     def disconnect(self, close_code):
         """
         Handles the WebSocket disconnect event.
+        
+        On disconnecting:
+        1. Removes the connection from the associated chat group.
+        2. Calls the parent class's disconnect method to handle any additional disconnection logic.
 
         Args:
             close_code (int): The code signifying the reason for the disconnection.
-            
-        This method is called when a WebSocket connection is terminated. Cleanup or
-        additional disconnect logic can be added here if needed.
         """
-        pass
+        # Remove this connection from the chat group
+        async_to_sync(self.channel_layer.group_discard)(self.channel_id, self.channel_name)
+        
+        # Handle additional disconnection logic from the parent class
+        super().disconnect(close_code)
 
- 
+
+
+                #   CONSUMER SUMMARY
+"""The WebChatConsumer is a WebSocket consumer designed for real-time
+chat using Django Channels. It's constructed to receive, save, 
+and broadcast chat messages in real time:
+
+Initialization (__init__): When instantiated, the consumer initializes
+two attributes: channel_id and user to None.
+
+Connection (connect): On a WebSocket connection request:
+
+The connection is accepted.
+The channel_id is extracted from the URL route.
+A user (currently hardcoded) is associated with the connection.
+The connection joins a chat group associated with the channel_id.
+Receiving Messages (receive_json): When a message is received from 
+the frontend:
+
+The message's text is extracted.
+If the text is valid, the message is saved to the database.
+The saved message is then broadcasted to all clients in the 
+associated chat room.
+
+Broadcasting (_broadcast_message_to_group): A utility method
+is used to send the saved message to all members of the chat group.
+
+Disconnection (disconnect): It provides a placeholder to handle
+any logic that might be needed when the WebSocket connection is terminated.
+
+This consumer provides a basic implementation for real-time chat
+functionality, and its methods have been refactored for better 
+readability and maintainability."""
--- backend/gaff_project/webchat/models.py
+++ backend/gaff_project/webchat/models.py
@@ -1,15 +1,31 @@
 from enum import auto
 from django.db import models
-
 from django.contrib.auth import get_user_model
 
 
 class Conversation(models.Model):
+    """
+    A chat channel between users.
+    
+    Attributes:
+    - channel_id: Distinct identifier for each conversation.
+    - created_at: Date and time marking the start of the conversation.
+    """
     channel_id = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+
 class Message(models.Model):
-    Conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="message")
+    """
+    An individual message within a Conversation.
+    
+    Attributes:
+    - conversation: The chat channel this message belongs to.
+    - sender: The user responsible for this message.
+    - content: The actual text of the message.
+    - timestamp: Date and time marking when the message was dispatched.
+    """
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
     sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
