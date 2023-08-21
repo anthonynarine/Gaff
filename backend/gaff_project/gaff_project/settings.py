6077787142b3fcdfda2f0738983ae0b307b1904b
anthonynarine <anarine83@gmail.com>
2023-08-21 15:58:21 -0400

    jwtinterceptor updated to send a request to the be using the refresh token to get a new access token. I've also set this up to get a new refreshe token

--- backend/gaff_project/gaff_project/settings.py
+++ backend/gaff_project/gaff_project/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -33,6 +34,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -124,7 +126,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework.authentication.SessionAuthentication",
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 
@@ -138,9 +140,14 @@ SPECTACULAR_SETTINGS = {
 }
 
 
-#....ADDED ....
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+# ....ADDED ....
+CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
+
+# ...ADDED...
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=5),  #TESTING TIMER
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "ALGORITHM": "HS384",
 }
